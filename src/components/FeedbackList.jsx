d95a0b10ad5ec96b985ec806ff2cc533b6efd929
ViniUK00 <elvin.bilo@icloud.com>
2022-12-02 22:03:08 +0000

    added Header,ListItem components and styles.

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import FeedBackItem from "./FeedbackItem"
+
+function FeedbackList({ feedback }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feetback Yet.</p>
+  }
+
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedBackItem key={item.id} item={item} />
+      ))}
+    </div>
+  )
+}
+
+export default FeedbackList
