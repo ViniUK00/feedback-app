222784f94be1bb6b8e504518040093c380dfea68
ViniUK00 <elvin.bilo@icloud.com>
2022-12-08 21:32:27 +0000

    added Feedback stats (how many reviews & whats the avg rating)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import Card from "./components/shared/Card"
+import FeedbackStats from "./components/FeedbackStats"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -17,6 +18,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
